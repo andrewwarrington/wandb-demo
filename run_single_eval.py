85f040107db22db180a47cde4971ae1d60060fb2
andrewwarrington <awarring@stanford.edu>
2023-02-13 15:21:51 -0800

    add cmd line interface

--- run_single_eval.py
+++ run_single_eval.py
@@ -54,11 +54,12 @@ plt.xlabel('$x$')
 
 # Set up the experiment.
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', default=1e-03, type=float)
-parser.add_argument('--optimizer', default='adam', type=str)
-parser.add_argument('--opt_steps', default=1_000, type=int)
-parser.add_argument('--seed', default=0, type=int)
-parser.add_argument('--wandb_project', default='wandb-demo', type=str)
+parser.add_argument('--bias', type=float, help='bias parameter.')
+parser.add_argument('--linear', type=float, help='linear parameter.')
+parser.add_argument('--quad', type=float, help='quadratic parameter.')
+parser.add_argument('--cubic', type=float, help='cubic parameter.')
+
+parser.add_argument('--wandb_project', default='wandb-demo-2', type=str)
 parser.add_argument('--wandb_entity', default='andrewwarrington', type=str)
 config_raw = vars(parser.parse_args())
 
@@ -76,16 +77,15 @@ except:
 	print('[WARNING]: Failed to grab git info...')
 
 
-# Learning args.
-learning_args = {
-	'lr': config_raw['lr'],
-	'optimizer': config_raw['optimizer'],
-	'opt_steps': config_raw['opt_steps'],
+parameter_args = {
+	'bias': config_raw['bias'],
+	'linear': config_raw['linear'],
+	'quad': config_raw['quad'],
+	'cubic': config_raw['cubic'],
 }
 
 # Args for the general execution.
 environment_args = {
-	'seed': config_raw['seed'],
 	'n_points': x_points.shape[0],
 	'git_commit': git_commit,
 	'git_branch': git_branch,
@@ -93,8 +93,8 @@ environment_args = {
 }
 
 # Merge the args.
-wnb_args = deepcopy(learning_args)
-wnb_args.update(deepcopy(environment_args))
+wnb_args = deepcopy(environment_args)
+wnb_args.update(deepcopy(parameter_args))
 
 # Make the WandB object.
 wnb = wandb.init(project=config_raw['wandb_project'], entity=config_raw['wandb_entity'], config=wnb_args)
@@ -110,80 +110,36 @@ def loss(params):
 	return loss.mean()
 
 
-@jax.jit
-def train_step(params, opt_state):
-	loss_value, grads = jax.value_and_grad(loss)(params)  # Can do minibatching in here.
-	updates, opt_state = optimizer.update(grads, opt_state, params)
-	params = optax.apply_updates(params, updates)
-	return params, opt_state, loss_value
-
-
 # Set up the initial model.
-key = jax.random.PRNGKey(environment_args['seed'])
-key, *subkeys = jax.random.split(key, num=5)
-model_initial_args = {
-	'bias': jax.random.normal(key=subkeys[0]),
-	'linear': jax.random.normal(key=subkeys[1]),
-	'quad': jax.random.normal(key=subkeys[2]),
-	'cubic': jax.random.normal(key=subkeys[3]),
-}
-test_model = foo(**model_initial_args)
-model_args = deepcopy(model_initial_args)
+test_model = foo(**parameter_args)
 
-# Set up the optimizer.
-if learning_args['optimizer'] == 'adam':
-	optimizer = optax.adam(learning_rate=learning_args['lr'])
-elif learning_args['optimizer'] == 'sgd':
-	optimizer = optax.sgd(learning_rate=learning_args['lr'])
-else:
-	raise NotImplementedError()
 
-opt_state = optimizer.init(model_initial_args)
+# Save a figure for shits and giggles.
+plt.figure(figsize=(6, 6))
+plt.scatter(x_points, y_points, c='tab:blue', label='Observed points.')
+plt.scatter(x_points, test_model.predict(x_points), c='tab:orange', label='Predicted points.')
+plt.grid(True)
+plt.ylabel('$2 x^3 + 3 x^2 + 4x + 5$')
+plt.xlabel('$x$')
+plt.savefig('./predictions_final.png', dpi=300)
+wnb.log({'predictions_final': wandb.Image("predictions_final.png")})
 
-# Main training loop.
-for step in range(learning_args['opt_steps']):
-	model_args, opt_state, loss_value = train_step(model_args, opt_state)
-
-	if step % 100 == 0:
-		# Evaluate the model.
-		print(f'Step: {step:> 4d}: loss: {loss_value:> 6.4f}')
-		wnb.log({'losses/loss': float(loss_value)}, commit=False, step=step)
-
-		# Save a figure for shits and giggles.
-		plt.figure(figsize=(6, 6))
-		plt.scatter(x_points, y_points, c='tab:blue', label='Observed points.')
-		plt.scatter(x_points, foo(**model_args).predict(x_points), c='tab:orange', label='Predicted points.')
-		plt.grid(True)
-		plt.ylabel('$2 x^3 + 3 x^2 + 4x + 5$')
-		plt.xlabel('$x$')
-		plt.savefig('./predictions_final.png', dpi=300)
-		wnb.log({'predictions_final': wandb.Image("predictions_final.png")}, step=step)
-
-		# Push everything.
-		wnb.log({}, commit=True, step=step)
 
 # Do some final evaluation and stuff...
-final_loss = loss(model_args)
+final_loss = loss(parameter_args)
 wnb.summary['losses/final_loss'] = final_loss
 
+
 # Save the predictions by logging directly.
-yp = foo(**model_args).predict(x_points)
+yp = foo(**parameter_args).predict(x_points)
 with open('./predictions_raw.p', 'wb') as f:
 	pickle.dump({'x': x_points,
 				 'y': y_points,
 				 'yp': yp}, f)
 wnb.save('./predictions_raw.p')
 
-# Push anything leftover.
-wnb.log({}, commit=True)
 
 # Close up.
 # This sometimes confuses WandB in notebooks and isn't actually required.
 wnb.finish()
-
-
-
-
-
-
 
